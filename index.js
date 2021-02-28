dbe4878a5427d32eba77066f43b5f825a16c3a87
Always_Empty <47966951+sweetcorn1229@users.noreply.github.com>
2021-02-28 12:47:27 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
-function _ByteArray (size) {
+function ByteArray (size) {
   let _Array = java.lang.reflect.Array;
   let _Byte = java.lang.Byte;
   return _Array.newInstance(_Byte.TYPE, size);
-}
-
-if (this.module) {
-  module.exports = {
-    ByteArray: _ByteArray
-  };
 }
