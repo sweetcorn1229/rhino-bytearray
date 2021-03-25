2d90c0842ec9b3ddaaa4c09e557e4d750de6e26a
Always_Empty <47966951+sweetcorn1229@users.noreply.github.com>
2021-03-25 16:25:07 +0900

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ function ByteArray (size) {
   let _Array = java.lang.reflect.Array;
   let _Byte = java.lang.Byte;
   this.core = _Array.newInstance(_Byte.TYPE, size);
+  return size;
 }
 
 ByteArray.prototype.init = function () {
